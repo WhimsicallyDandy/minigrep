50a75124480a0f693383cd4a3900bd67079f3a7f
Dandy <drjelly606@gmail.com>
2020-04-16 14:05:58 +1000

    Initial commit, at 12-4, moving functionality from main.rs to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use std::fs;
+use std::error::Error;
+
+// returns a Result, which means we can do things with the Error
+// the second Result type is a type that implements the Error trait.
+// we don't have to specify what the return type will exactly be.
+// so we can be more flexible with return Error values
+// dyn is short for dynamic.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // so the ? means it will return the Error from read_to_string
+    // rather than .expect() and our own error handling
+    let contents = fs::read_to_string(config.filename)?;    
+
+    // return the unit type (), which i think is just nothing?
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    // takes a reference to a vector of strings to create a struct of String objects (not slices)!
+    // returns a Result<Config, str> which is important
+    // &'static str is the type of string literals!
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        // default constructor, order as arguments appear in code
+        Ok(Config { query, filename })
+    }
+
+    // Extracting the cmd line arguments into a specific configuration.
+    // Uses owned clones rather than string slices, which are references.
+    // This is an easier, but a little more inefficient method.
+    // However, because we clone the data, we don't need to manage lifetimes
+    // so it's a trade-off of simplicity vs performance. And in this case it's
+    // worthwhile.
+    // the query and filename strings are likely to be very small.
+    // as always: Working --then--> Efficiency
+}
+
+// we'll be returning a substring of "contents"
+// so we need to guarantee the function that it's still around.
+// The return data will last as long as contents
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::process;
+
+
+use minigrep::Config;
+
+
+fn main() {
+    // turns cmd line arguments into a String Vector. Need to specify type, as collect can do a lot of data structures
+    let args: Vec<String> = env::args().collect();
+    // debug formatting is {:?} for vectors and other collections
+    // &args[0] is the binary executable name
+
+    // unwrap_or_else will unwrap the Ok value
+    // otherwise, it will perform the code in this anonymous function
+    // with the err in vertical bars (|err|) containing the value
+    // passed in the Err from the Config constructor
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // execute run(), and if there is an Error, do this
+    // used instead of above "let config = " because we don't care about
+    // keeping a return value, as a successful execution just returns "()"
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+    
+}
+
